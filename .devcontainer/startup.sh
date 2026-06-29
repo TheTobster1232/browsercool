82190c9499e318c41b7cac80a66c8cc3c1da4035
TheTobster1232 <151861999+TheTobster1232@users.noreply.github.com>
2026-06-29 10:44:28 +0100

    Configure VNC server in startup script
    Set up VNC server with appropriate configuration.

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -9,3 +9,9 @@ websockify --web=/usr/share/novnc/ 6080 localhost:5901
 
 chmod +x startup.sh
 
+export USER=root
+echo "xfce4-session" > ~/.vnc/xstartup
+chmod +x ~/.vnc/xstartup
+vncserver -kill :1 2>/dev/null
+vncserver :1 -geometry 1920x1080
+websockify --web=/usr/share/novnc/ 6080 localhost:5901
