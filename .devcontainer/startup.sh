4ceaba4cb8fa08bf02d3d4429bf663371dc39352
TheTobster1232 <151861999+TheTobster1232@users.noreply.github.com>
2026-06-29 10:22:49 +0100

    Update startup.sh

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,3 +1,8 @@
+export USER=root
+vncserver -kill :1 2>/dev/null
+vncserver :1 -geometry 1920x1080
+websockify --web=/usr/share/novnc/ 6080 localhost:5901
+
 #!/bin/bash
 vncserver :1
 websockify --web=/usr/share/novnc/ 6080 localhost:5901
